d07df8c56fa2f4700abb8aa173cc6f4ac4f08eda
berker <berkerevl@gmail.com>
2023-10-25 04:05:21 +0300

    feat: character entity

--- src/character/entities/chararcter.cs
+++ src/character/entities/chararcter.cs
@@ -4,13 +4,49 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace DotNetAPI.Players.Entities
 {
+    public enum CharacterClass
+    {
+        Rogue,
+        Warrior,
+        Mage,
+        Archer,
+    }
+
     public class Character
     {
         [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
+        [BsonRepresentation(BsonType.String)]
+        public string CharacterId { get; set; } = Guid.NewGuid().ToString();
 
-        [BsonElement("Health")]
-        public int Health { get; set; }
+        [BsonElement("Class")]
+        [BsonRequired]
+        public CharacterClass Class { get; set; }
+
+        [BsonElement("MaxHealth")]
+        [BsonRequired]
+        public int MaxHealth { get; set; }
+
+        [BsonElement("CharMoney")]
+        [BsonRequired]
+        public double CharMoney { get; set; }
+
+        [BsonElement("Attack")]
+        public int Attack { get; set; }
+
+        [BsonElement("Defense")]
+        public int Defense { get; set; }
+
+        [BsonElement("Level")]
+        public int Level { get; set; }
+
+        public Character(CharacterClass characterClass, int maxHealth, double charMoney, int attack, int defense, int level)
+        {
+            Class = characterClass;
+            MaxHealth = maxHealth;
+            CharMoney = charMoney;
+            Attack = attack;
+            Defense = defense;
+            Level = level;
+        }
     }
 }
